200f8db4fd5d04d284162fe495ddaadbec4ed85c
Humberto Prado <betoxf@hotmail.com>
2025-12-28 13:18:04 +0900

    Initial Commit

---
+++ ClaudeUsageBar/ClaudeUsageBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClaudeUsageBarApp.swift
+//  ClaudeUsageBar
+//
+//  Created by Humberto Prado on 28/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ClaudeUsageBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
